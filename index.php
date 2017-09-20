eb6e3ff7c8fdafbd8aa29651a162b1261517bd10
Dylan Rumph <dprumph@gmail.com>
2017-09-19 22:14:41 -0500

    edited styling and added error case

--- index.php
+++ index.php
@@ -11,48 +11,60 @@ Project: Project 2 - Web Form Submission Application
         <title>DWA 15 Project 2</title>
         <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
         <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
-        <!--<link rel="stylesheet" type="text/css" href="project2.css">-->
+        <link rel="stylesheet" type="text/css" href="project2.css">
     </head>
     <body>
-        <h1>Pasta Cooking Guide</h1>
+        <h1 class='text-center'>Pasta Cooking Guide</h1>
+        <div class='container'>
+            <h4>This is an easy guide to help you cook perfect pasta.  Simply fill out the form below with
+            some basic information about the pasta you would like to cook, how much you plan to cook and what
+            kind of cosistency you would like the pasta to have.</h4>
+        </div>
         <!-- User Input Form -->
-        <form method='POST' action='pastaInfo.php'>
-            <!-- Drop down menu to select the type of pasta the user is cooking -->
-            <div class='form-group'>
-                <label for='pasta'>Select which type of pasta you want to cook</label>
-                <select name='pasta' id='pasta'>
-                    <option value='choose'>Choose a pasta...</option>
-                    <option value='penne'>Penne</option>
-                    <option value='rigatoni'>Rigatoni</option>
-                    <option value='rotini'>Rotini</option>
-                    <option value='ziti'>Ziti</option>
-                    <option value='spaghetti'>Spaghetti</option>
-                    <option value='vermicelli'>Vermicelli</option>
-                    <option value='fettuccine'>Fettuccine</option>
-                    <option value='linguine'>Linguine</option>
-                    <option value='farfalle'>Farfalle</option>
-                </select>
+        <div class='container'>
+            <div class='panel panel-info'>
+                <div class="panel-heading"></div>
+                <div class="panel-body">
+                    <form method='POST' action='pastaInfo.php'>
+                        <!-- Drop down menu to select the type of pasta the user is cooking -->
+                        <div class='form-group'>
+                            <label for='pasta'>Select which type of pasta you want to cook</label>
+                            <select name='pasta' id='pasta'>
+                                <option value='choose'>Choose a pasta...</option>
+                                <option value='penne'>Penne</option>
+                                <option value='rigatoni'>Rigatoni</option>
+                                <option value='rotini'>Rotini</option>
+                                <option value='ziti'>Ziti</option>
+                                <option value='spaghetti'>Spaghetti</option>
+                                <option value='vermicelli'>Vermicelli</option>
+                                <option value='fettuccine'>Fettuccine</option>
+                                <option value='linguine'>Linguine</option>
+                                <option value='farfalle'>Farfalle</option>
+                            </select>
+                        </div>
+                        <!--
+                        Numerical input for the amount of pasta to be cooked
+                        Value is set to default of 8 and must be between 1-32
+                        -->
+                        <div class='form-group'>
+                            <label for='quantity'> Ounces of Pasta to be cooked (1-32): </label>
+                            <input type='number' name='quantity' min='1' max='32' value='8'>
+                        </div>
+                        <!-- Radio button for the type of cook on the pasta, defaults to al dente -->
+                        <div class='form-group'>
+                            <fieldset class='radios'>
+                                <legend>What consistency would you like the pasta?</legend>
+                                <label><input type='radio' name='cook' value='al dente' checked='checked'> Al dente</label>
+                                <label><input type='radio' name='cook' value='soft'> Soft</label>
+                            </fieldset>
+                        </div>
+                        <!-- Form submission buttion -->
+                        <div class='form-group'>
+                            <input type='submit' class='btn btn-primary btn-sm'>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <!--
-            Numerical input for the amount of pasta to be cooked
-            Value is set to default of 8 and must be between 1-32
-            -->
-            <div class='form-group'>
-                <label for='quantity'> Ounces of Pasta (1-32): </label>
-                <input type='number' name='quantity' min='1' max='32' value='8'>
-            </div>
-            <!-- Radio button for the type of cook on the pasta, defaults to al dente -->
-            <div class='form-group'>
-                <fieldset class='radios'>
-                    <legend>How would you like your pasta cooked?</legend>
-                    <label><input type='radio' name='cook' value='al dente' checked='checked'> Al dente</label>
-                    <label><input type='radio' name='cook' value='soft'> Soft</label>
-                </fieldset>
-            </div>
-            <!-- Form submission buttion -->
-            <div class='form-group'>
-                <input type='submit' class='btn btn-primary btn-sm'>
-            </div>
-        </form>
+        </div>
     </body>
 </html>
--- pastaInfo.php
+++ pastaInfo.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <?php
 require('helpers.php');
+
 $pasta = '';
+$break = false;
+$time1 = 1;
+$time2 = 2;
+$div = 1;
 
 if (isset($_POST['pasta'])) {
     $pasta = $_POST['pasta'];
-    if ($pasta == 'choose') {
-        $alertType = 'alert-danger';
-        $results = 'Please choose a pasta.';
-    } elseif ($pasta == 'farfalle') {
+    if($pasta == 'choose'){
+        $break = true;
+    }elseif ($pasta == 'farfalle') {
         $time1 = 13;
         $time2 = 15;
         $div = 2.5;
@@ -73,6 +77,7 @@ if (isset($_POST['quantity'])) {
 
 $yield = $quantity / $div;
 ?>
+
 <html lang="en">
     <head>
         <meta charset="utf-8" />
@@ -81,12 +86,21 @@ $yield = $quantity / $div;
         <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
     </head>
     <body>
-        <h1>Pasta Cooking Instructions</h1>
+        <?php if($break): ?>
+            <div class="alert alert-danger">
+                <strong>Error!</strong> You did not choose a type of pasta.
+            </div>
+        <?php else: ?>
+            <div class='container'>
+                <div class='panel panel-info'>
+                    <div class="panel-heading"><h1 class='text-center'>Pasta Cooking Instructions</h1></div>
+
         <h3>To cook <?=$quantity?> ounces of <?=$pasta?> with <?=$cook?> consistency,
             you will first need to bring <?=$water?> quarts of very salty water to a hard boil.
             Next, you add in the full amount of pasta while occasionally stirring for the first two minutes.
             Remember to keep the heat high so that water stays at a hard boil the entire time.
             <?=$pasta?> with a <?=$cook?> consistency will take <?=$time?> total minutes to finish cooking.
             Once this time has passed reserve 1 cup of the pasta water and then strain the pasta.
-            Place the pasta into a large sauce pan and combine with reserved pasta water and warmed pasta sauce.
-            You will have approximately <?=$yield?> servings of <?=$pasta?> to enjoy! </h2>
+            Place the pasta into a large sauce pan and combine with reserved pasta water and warmed pasta sauce. Heat
+            together for 2 minutes. You will have approximately <?=$yield?> servings of <?=$pasta?> to enjoy! </h2>
+        <?php endif; ?>
